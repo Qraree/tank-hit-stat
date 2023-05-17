da5e1da50311c7cc575996404c53f6cb4b1c4e28
Qraree <metbezu@gmail.com>
2023-05-17 23:27:55 +0300

    feat/added tank info

--- renderer.js
+++ renderer.js
@@ -29,14 +29,14 @@ const T80_ARMOR = {
     FRONT: {
         ARMOR_PATH: 'assets/armors/t-80/t-80-pered.json',
         PHOTO_PATH: 'assets/armors/t-80/tank_pered.png',
-        WIDTH: 3800,
-        HEIGHT: 2885,
+        WIDTH: 3630,
+        HEIGHT: 2682,
     },
     SIDE: {
         ARMOR_PATH: 'assets/armors/t-80/t-80-side-armor.json',
         PHOTO_PATH: 'assets/armors/t-80/t-80-side.png',
-        WIDTH: 7200,
-        HEIGHT: 2885,
+        WIDTH: 7012,
+        HEIGHT: 2882,
     }
 }
 
@@ -44,14 +44,14 @@ const ABRAMS_ARMOR = {
     FRONT: {
         ARMOR_PATH: 'assets/armors/abrams/abrams-pered.json',
         PHOTO_PATH: 'assets/armors/abrams/abrams_pered.png',
-        WIDTH: 3530,
-        HEIGHT: 2885,
+        WIDTH: 3654,
+        HEIGHT: 3000,
     },
     SIDE: {
         ARMOR_PATH: 'assets/armors/abrams/abrams-bok.json',
         PHOTO_PATH: 'assets/armors/abrams/abrams_bok.png',
-        WIDTH: 7200,
-        HEIGHT: 2885,
+        WIDTH: 7610,
+        HEIGHT: 3000,
     }
 }
 
@@ -59,14 +59,14 @@ const LEOPARD_ARMOR = {
     FRONT: {
         ARMOR_PATH: 'assets/armors/leopard/leopard-pered-armor.json',
         PHOTO_PATH: 'assets/armors/leopard/leopard_pered.png',
-        WIDTH: 3630,
-        HEIGHT: 2885,
+        WIDTH: 3700,
+        HEIGHT: 2864,
     },
     SIDE: {
         ARMOR_PATH: 'assets/armors/leopard/leopard-side-armor.json',
         PHOTO_PATH: 'assets/armors/leopard/leopard_bok.png',
-        WIDTH: 7200,
-        HEIGHT: 2885,
+        WIDTH: 7672,
+        HEIGHT: 2864,
     }
 }
 
